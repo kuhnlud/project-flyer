0ee5033f42054160ea48450ab92b5543b3980be5
kuhnlud <ludovic.kuhn@gmail.com>
2015-10-16 18:12:10 +0200

    Initial Work -Gulp, gitignore-

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Project Flyer</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+
+    <div class="container">
+
+        @yield('content')
+
+    </div>
+</body>
+</html>
